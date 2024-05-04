7d446829de1dc2a95d57c481dfcac820677914dd
Karzhavin Nikita <kar.ni.o@icloud.com>
2024-05-04 17:13:09 +0500

    Added stack tests

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -4,8 +4,35 @@ require_relative 'test_helper'
 require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
+  def setup
+    @stack = Stack.new
+    @stack.push!('ruby')
+    @stack.push!('python')
+    @stack.push!('go')
+  end
+
+  def teardown; end
+
   # BEGIN
-  
+  def test_stack_push
+    @stack.push!('elixir')
+    assert_equal @stack.size, 4
+    assert_equal @stack.empty?, false
+    assert_equal @stack.to_a, ['ruby', 'python', 'go', 'elixir']
+  end
+
+  def test_stack_pop
+    @stack.pop!
+    assert_equal @stack.size, 2
+    assert_equal @stack.empty?, false
+    assert_equal @stack.to_a, ['ruby', 'python']
+  end
+
+  def test_stack_clear
+    @stack.clear!
+    assert_equal @stack.to_a, []
+    assert_equal @stack.empty?, true
+  end
   # END
 end
 
